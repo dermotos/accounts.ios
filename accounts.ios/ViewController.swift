db251ba7e0490a539c2a273a0efdd4e7fead79d9
Dermot O Sullivan <me@dermotos.com>
2018-01-21 11:12:59 +1100

    Initial Commit

---
+++ accounts.ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  accounts.ios
+//
+//  Created by Dermot O Sullivan on 21/1/18.
+//  Copyright © 2018 Rocky Desk Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
